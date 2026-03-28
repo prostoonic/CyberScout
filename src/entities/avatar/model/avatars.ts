7d1b02d14cd74e631803d56565f1e9fd4e1808c5
prostoonic <artem.stralenia@gmail.com>
2026-03-28 22:18:39 +0300

    generate new avatars

--- src/entities/avatar/model/avatars.ts
+++ src/entities/avatar/model/avatars.ts
@@ -1,26 +1,26 @@
 export const AVATARS = [
   {
     id: 1,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar1.svg',
   },
   {
     id: 2,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar2.svg',
   },
   {
     id: 3,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar3.svg',
   },
   {
     id: 4,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar4.svg',
   },
   {
     id: 5,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar5.svg',
   },
   {
     id: 6,
-    image: '/avatars/test.svg',
+    image: '/avatars/Avatar6.svg',
   },
 ]
