2ba3f137f4e4b1fc72eda6d40115f16a6edcaef5
prostoonic <artem.stralenia@gmail.com>
2026-04-02 19:56:31 +0300

    smal fix ui/ux

--- src/widgets/ui/footer/Footer.tsx
+++ src/widgets/ui/footer/Footer.tsx
@@ -1,11 +1,12 @@
 import styles from './footer.module.scss'
+import Link from 'next/link'
 
 export function Footer() {
   return (
     <footer className={styles.footer}>
       <p className={styles.text}>
         Проект выполнен в рамках конкурса{' '}
-        <span className={styles.tag}>#КиберПраво</span>
+        <Link href="https://mvd.gov.by/ru/news/17422" target="_blank" rel="noopener noreferrer" className={styles.tag}>#КиберПраво</Link>
       </p>
     </footer>
   )
